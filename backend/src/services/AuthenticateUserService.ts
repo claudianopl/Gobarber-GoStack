8076450080b57459e0f051196189a50ef7343c77
Claudiano Lima <claudianopereira047@gmail.com>
2020-11-21 15:06:04 -0300

    Criando autenticação do usuário com JWT.

---
+++ backend/src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { sign } from 'jsonwebtoken';
+
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, 'f9770c0e05276335774c156c4e7a7542', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
