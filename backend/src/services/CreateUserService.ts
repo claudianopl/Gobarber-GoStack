9f576e429a107f26e7102b575d73c5b757d55c65
Claudiano Lima <claudianopereira047@gmail.com>
2020-11-25 00:47:53 -0300

    Tratamento de Exceções

--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface RequestDTO {
   name: string;
@@ -18,7 +19,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('Email address alread used');
+      throw new AppError('Email address alread used');
     }
 
     const hashPassword = await hash(password, 8);
