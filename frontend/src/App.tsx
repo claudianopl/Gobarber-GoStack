b9669a7e7bb72ea0a5cb877cda038c6cc2cd9414
Claudiano Lima <claudianopereira047@gmail.com>
2020-12-10 01:04:12 -0300

    Página de cadastro.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import SingIn from './pages/SignIn/index';
+// import SingIn from './pages/SignIn/index';
+import SingUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SingIn />
+    <SingUp />
     <GlobalStyle />
   </>
 );
---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/input/index';
+import Button from '../../components/button/index';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="login">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
