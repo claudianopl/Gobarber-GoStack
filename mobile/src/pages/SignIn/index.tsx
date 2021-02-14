7d565ce54384ee9385d3b61cd8fc87b42d7d82de
Claudiano Lima <claudianopereira047@gmail.com>
2021-02-14 12:40:05 -0300

    06 - Importando fontes externas

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SingIn: React.FC = () => {
   return (
-    <Container/>
-  )
-}
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
+};
 
 export default SingIn;
