792db8427b4c69f25779087bc8c00a4ab31c6ce5
Claudiano Lima <claudianopereira047@gmail.com>
2021-02-16 20:01:58 -0300

    03 - Rotas privadas

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -3,10 +3,10 @@ import { View, Button } from 'react-native';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  const { singOut } = useAuth()
+  const { signOut } = useAuth()
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title='Sair' onPress={singOut} />
+      <Button title='Sair' onPress={signOut} />
     </View>
   );
 }
