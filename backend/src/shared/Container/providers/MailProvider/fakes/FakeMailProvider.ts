7e5629090df2cb9b2f194fd63e446fbc2939fced
Claudiano Lima <claudianopereira047@gmail.com>
2021-04-04 16:19:52 -0300

    02 - Perfil do Usuário/07 - Template de emails

--- backend/src/shared/Container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ backend/src/shared/Container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -1,17 +1,10 @@
+import ISendMailDTO from '../dtos/ISendMailDTO';
 import IMailProvider from '../models/IMailProvider';
 
-interface IMessage {
-  to: string;
-  body: string;
-}
-
 export default class FakeMailProvider implements IMailProvider {
-  private messeges: IMessage[] = [];
+  private messeges: ISendMailDTO[] = [];
 
-  public async sendMail(to: string, body: string): Promise<void> {
-    this.messeges.push({
-      to,
-      body,
-    });
+  public async sendMail(message: ISendMailDTO): Promise<void> {
+    this.messeges.push(message);
   }
 }
